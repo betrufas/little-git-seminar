9864530282e6ec3431804ea511154db8f7ec930d
albert752 <azemar.albert@gmail.com>
2019-03-21 14:49:43 +0100

    Started class

---
+++ main.py
@@ -0,0 +1,17 @@
+class Pelakas :
+    def __init__(self):
+        pass
+    def Bet ():
+        return
+    def Pau ():
+        return
+    def Eric ():
+        return
+    def Molina ():
+        return
+    def POl ():
+        return
+    def Azemar ():
+        return
+    def Dimas ():
+        return
